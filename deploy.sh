068d71eee7ad0c620ae0b93231a26e12d2b84d38
BulatSaif <bulat@saifullin.ru>
2017-04-28 00:11:38 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-
-ssh  -i dd testuser@104.155.104.120 << EOF
+ssh-keyscan -t rsa -H 104.155.104.120 >> ~/.ssh/known_hosts
+ssh -oStrictHostKeyChecking=no -i dd testuser@104.155.104.120 << EOF
 
 pwd
 ls
-
 EOF
